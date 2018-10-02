c8c7c0ced3f1625099cde13fc174c9d16ce95e49
Christopher Fung <chrisfung2897@hotmail.com>
2018-10-02 22:45:06 +1000

    Added REST service class and corresponding function to filter orders in History, only works if xml does not link to xsd so still need to work on it. Modified web.xml for Jersey. Modified xml files so that they use the commonTypes properly

--- src/java/uts/wsd/oms/History.java
+++ src/java/uts/wsd/oms/History.java
@@ -25,8 +25,74 @@ public class History implements Serializable {
     public void setOrders(ArrayList<Order> orders) {
         this.orders = orders;
     }
-    
-    public void addOrder(Order order){
+
+    public void addOrder(Order order) {
         orders.add(order);
+    }
+
+    public List<Order> getOrdersByParams(int id, String email, String title, String status) {
+        List<Order> ordersToReturn = new ArrayList<Order>();
+
+        //Add orders matching given ID (unless no ID given)
+        if (id != -1) {
+            for (Order order : orders) {
+                if (order.getOrderID() == id) {
+                    ordersToReturn.add(order);
+                }
+            }
+        } else {
+            ordersToReturn.addAll(orders);
+        }
+
+        //Remove any orders that do match email parameter
+        if (email != null) {
+            List<Order> toDelete = new ArrayList<Order>();
+
+            for (Order order : ordersToReturn) {
+                if (!order.getEmail().equals(email)) {
+                    toDelete.add(order);
+                }
+            }
+
+            ordersToReturn.removeAll(toDelete);
+        }
+
+        //Remove any orders that do match title parameter
+        if (title != null) {
+            List<Order> toDelete = new ArrayList<Order>();
+
+            for (Order order : ordersToReturn) {
+                List<Movie> movies = order.getMovies().getList();
+                
+                int i = 0;
+                boolean isMatch = false;
+                while (i < movies.size() && !isMatch) {
+                    if (movies.get(i).getTitle().equals(title)) {
+                        isMatch = true;
+                    }
+                }
+                
+                if (!isMatch) {
+                    toDelete.add(order);
+                }
+            }
+
+            ordersToReturn.removeAll(toDelete);
+        }
+
+        //Remove any orders that do match status parameter
+        if (status != null) {
+            List<Order> toDelete = new ArrayList<Order>();
+
+            for (Order order : ordersToReturn) {
+                if (!order.getOrderStatus().equals(status)) {
+                    toDelete.add(order);
+                }
+            }
+
+            ordersToReturn.removeAll(toDelete);
+        }
+
+        return ordersToReturn;
     }
 }
---
+++ src/java/uts/wsd/oms/rest/HistoryService.java
@@ -0,0 +1,45 @@
+package uts.wsd.oms.rest;
+        
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.ServletContext;
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
+import javax.xml.bind.JAXBException;
+import uts.wsd.oms.*;
+
+@Path("/movieStoreApp")
+public class HistoryService {
+    
+    @Context
+    private ServletContext application;
+
+    private MovieStoreApplication getMovieStoreApp() throws JAXBException, IOException {
+        synchronized (application) {
+            MovieStoreApplication movieStoreApp = (MovieStoreApplication) application.getAttribute("movieStoreApp");
+            if (movieStoreApp == null) {
+                movieStoreApp = new MovieStoreApplication();
+                movieStoreApp.setFilePath(application.getRealPath("WEB-INF"));
+                application.setAttribute("movieStoreApp", movieStoreApp);
+            }
+            return movieStoreApp;
+        }
+    }
+    
+    @Path("history/all")
+    @GET
+    @Produces(MediaType.APPLICATION_XML)
+    public History getHistory() throws JAXBException, IOException {
+        return getMovieStoreApp().getHistory();
+    }
+    
+    @Path("history")
+    @GET
+    @Produces(MediaType.APPLICATION_XML)
+    public List<Order> getHistoryByParams(@DefaultValue("-1") @QueryParam("id") int id, @QueryParam("email") String email,
+            @QueryParam("title") String title, @QueryParam("status") String status) throws JAXBException, IOException {
+        
+        return getMovieStoreApp().getHistory().getOrdersByParams(id, email, title, status);
+    }
+}
