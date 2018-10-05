5b8818e713fef17d6820905fb46715151d5d209a
Caleb Ardern <caleb.ardern@assertiv.com>
2018-10-05 16:15:11 +1000

    Revert "Revert "Merge branch 'master' of https://bitbucket.org/wsdgroup1/wsd31284assignment""
    This reverts commit 22f73609418b12cb1eafdfea8dc7709965c3bdc8.

--- src/java/uts/wsd/oms/Util.java
+++ src/java/uts/wsd/oms/Util.java
@@ -5,17 +5,28 @@ import java.util.*;
 public class Util {
 
     private static int nextOrderID = 0;
+    private static ArrayList<Integer> usedIDs = new ArrayList<Integer>();
 
     public static int generateOrderID() {
         int temp = nextOrderID;
-        nextOrderID++;
+        generateNextID();
         return temp;
     }
 
     public static void initOrderID(History history) {
         if(history != null && history.getOrders() != null)
             for (Order order : history.getOrders())
-                if (order.getOrderID() > nextOrderID)
-                    nextOrderID = order.getOrderID() + 1;
+                usedIDs.add(order.getOrderID());
+        generateNextID();
+    }
+    
+    private static void generateNextID(){
+        int id;
+        Random rand = new Random();
+        int min = 100, max = 999;
+        do{
+            id = rand.nextInt((max - min) + 1) + min;
+        }while(usedIDs.contains(id));
+        nextOrderID = id;
     }
 }
