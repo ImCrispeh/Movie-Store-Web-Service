b64cddf970d6c9071b56121b67908e32dab09b3f
CalebArdern <caleb.ardern@gmail.com>
2018-10-08 16:48:23 +1100

    Major (WIP) future problems outline

--- src/java/uts/wsd/oms/soap/client/MovieStoreClient.java
+++ src/java/uts/wsd/oms/soap/client/MovieStoreClient.java
@@ -4,15 +4,21 @@ import java.util.Scanner;
 
 public class MovieStoreClient {
 
-    static Movie_Service locator = new Movie_Service();
-    static MovieSoap movie = locator.getMovieSoapPort();
+    static Movie_Service movielocator = new Movie_Service();
+    static MovieSoap movie = movielocator.getMovieSoapPort();
+    static PlaceOrder_Service Orderlocator = new PlaceOrder_Service();
+    static PlaceOrder order = Orderlocator.getPlaceOrderPort();
+    static Scanner in = new Scanner(System.in);
 
     public static void main(String[] args) {
+        AddMovie();
+        System.out.println("-------------------------------------------------------------");
+        PlaceOrder();
+        System.out.println("-------------------------------------------------------------");
         ViewOrders();
     }
 
     private static void ViewOrders() {
-        Scanner in = new Scanner(System.in);
         String email, id, title, status;
         System.out.print("Enter email: ");
         email = in.nextLine();
@@ -46,5 +52,34 @@ public class MovieStoreClient {
         orderString += "└ Sale Totale: $" + order.getSaleTotal() + "\n";
         orderString += "└ Order Status: " + order.getOrderStatus();
         return orderString;
+    }
+    private static void PlaceOrder() {
+        String email, firstName, lastName, status;
+        System.out.print("Enter email: ");
+        email = in.nextLine();
+        System.out.print("Enetr First Name: ");
+        firstName = in.nextLine();
+        System.out.print("Enter last Name: ");
+        lastName = in.nextLine();
+        
+        System.out.println("Finalising your order");
+        order.addOrder(email, firstName, lastName, (uts.wsd.oms.soap.client.Movies) movies.movie);
+    }
+    public static void AddMovie(){
+        Movie movie = new Movie();
+        System.out.println("Please enter the details for the movie you would like to Buy: ");
+        System.out.print("Enter the Title:");
+        movie.setTitle(in.nextLine());
+        System.out.print("Enter the Genre:");
+        movie.setGenre(in.nextLine());
+        System.out.print("Enter the release date of the movie:");
+        movie.setReleaseDate(in.nextInt());
+        System.out.print("Enter the amount of copies avalible:");
+        movie.setCopies(in.nextInt());
+        System.out.print("Enter the Price of each copie:");
+        movie.setPrice(in.nextInt());
+        
+        Movies movies = new Movies();
+        movies.movie.add(movies.movie.size(), movie);
     }
 }
